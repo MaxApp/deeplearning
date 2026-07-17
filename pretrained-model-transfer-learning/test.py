4de01a2d7f6cc37a69469fbfef44c2cfda7468a3
max <max2030tokyo@hotmail.com>
2026-07-17 20:12:03 +0800

    update scripts

--- pretrained-model-transfer-learning/test.py
+++ pretrained-model-transfer-learning/test.py
@@ -12,5 +12,5 @@ transform = transforms.Compose([
 # MNIST train data
 train_dataset = torchvision.datasets.MNIST(root="./data",train=True,download=False,transform=None)
 # train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
-# utils.print_gray_image_in_console(train_dataset[10][0])
-utils.plot_gray_image(train_dataset[10][0])
+utils.print_gray_image_in_console(train_dataset[3][0])
+# utils.plot_gray_image(train_dataset[9][0])
