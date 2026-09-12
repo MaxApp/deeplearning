3bfb47b5f7994eba4266acd6d836a45310b6e790
max <max2030tokyo@hotmail.com>
2026-09-12 19:30:50 +0800

    update siamese qa

--- Text-and-NLP/siamese_qa.py
+++ Text-and-NLP/siamese_qa.py
@@ -12,7 +12,6 @@ def TripletLossFn(v1, v2, margin=0.25):
         torch.Tensor: Triplet Loss.
     """
     
-    # Take the dot product of the two batches.
     scores = torch.matmul(v1, v2.transpose(0, 1))
     # calculate new batch size
     batch_size = len(scores)
@@ -20,16 +19,15 @@ def TripletLossFn(v1, v2, margin=0.25):
     eye = torch.eye(batch_size, device=scores.device, dtype=scores.dtype)
     negative_without_positive = scores - eye * 2.0
     # take the row by row `max` of `negative_without_positive`. 
-    # Hint: negative_without_positive.max(axis = [?])  
     closest_negative = negative_without_positive.max(dim=1).values
-    # Subtract the diagonal from the negative scores before averaging.
+    # subtract the diagonal from the negative scores before averaging.
     negative_zero_on_duplicate = (1.0 - eye) * scores
     mean_negative = torch.sum(negative_zero_on_duplicate, dim=1) / (batch_size - 1)
     # A = subtract `positive` from `margin` and add `closest_negative` 
     triplet_loss1 = torch.maximum(margin - positive + closest_negative, torch.zeros_like(positive))
     # B = subtract `positive` from `margin` and add `mean_negative`
     triplet_loss2 = torch.maximum(margin - positive + mean_negative, torch.zeros_like(positive))
-    # Add the two losses together and take their mean.
+    # add the two losses together and take their mean.
     triplet_loss = torch.mean(triplet_loss1 + triplet_loss2)
     
     return triplet_loss
