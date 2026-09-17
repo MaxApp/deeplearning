c80e28d705a9dd9b49b559572fbaaafa505dbf61
max <max2030tokyo@hotmail.com>
2026-09-17 13:58:54 +0800

    update position encoding

--- Text-and-NLP/position_encoding.py
+++ Text-and-NLP/position_encoding.py
@@ -6,6 +6,7 @@ def positional_encoding(seq_len, d_model):
     P = np.zeros((seq_len, d_model))
     # extend dimensions for broadcast
     pos = np.arange(seq_len)[:, np.newaxis]
+    # range from 0 to d_model//2 - 1
     i = np.arange(d_model // 2)
     denominator = n ** (2 * i / d_model)
     # even positions
@@ -23,3 +24,26 @@ if __name__ == "__main__":
     plt.ylabel("Position")
     plt.colorbar()
     plt.show()
+
+
+
+
+# other variants
+def get_angles(position, k, d_model):
+    i = k // 2
+    angle_rates = 1 / np.power(10000, (2 * i) / np.float32(d_model))
+    return position * angle_rates
+
+def positional_encoding2(positions, d_model): 
+    angle_rads = get_angles(np.arange(positions)[:, np.newaxis], 
+                            np.arange(d_model)[np.newaxis, :],
+                            d_model)
+  
+    # apply sin to even indices
+    angle_rads[:, 0::2] = np.sin(angle_rads[:, 0::2])
+    # apply cos to odd indices
+    angle_rads[:, 1::2] = np.cos(angle_rads[:, 1::2])
+    
+    pos_encoding = angle_rads[np.newaxis, ...]
+    return pos_encoding
+
